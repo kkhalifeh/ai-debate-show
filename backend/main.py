130165d1eb5fc4be9330fb2d41256af2525e02fd
kkhalifeh <khaled.khalifeh@gmail.com>
2025-06-23 11:25:47 +0300

    matching with server

--- backend/main.py
+++ backend/main.py
@@ -18,7 +18,8 @@ app = FastAPI()
 # Add CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://localhost:3000",
+                   "https://ammantv.atmata.ai", "http://ammantv.atmata.ai"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
